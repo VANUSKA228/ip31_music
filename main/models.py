5b21430b8ab843437edbcb9298361a25bab7f871
Ivan <decugeroi@gmail.com>
2026-03-26 09:40:21 +0700

    добавлена модель исполнителя

--- main/models.py
+++ main/models.py
@@ -7,11 +7,18 @@ class Genre(models.Model):
     description = models.TextField(verbose_name="Описание")
     def __str__(self):
         return self.name_ru
+    
+class Artist(models.Model)
+    name = models.CharField(max_length=500, unique=True)
+    image = models.ImageField(upload_to='artists/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 class Track(models.Model):
     title = models.CharField(max_length=500,unique=True)
     duration =models.DurationField()
     genres = models.ManyToManyField(Genre)
-
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT, null=True)
     def __str__(self):
         return self.title
